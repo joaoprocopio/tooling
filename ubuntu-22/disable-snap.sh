bd9a3c774466f8523c98bdcd8de7e31af930075a
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-06-14 00:40:37 -0300

    nosnap comes first

--- ubuntu-22/disable-snap.sh
+++ ubuntu-22/disable-snap.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 #
+echo 'Package: snapd
+Pin: release a=*
+Pin-Priority: -10' | sudo tee /etc/apt/preferences.d/nosnap.pref
+
+#
 sudo snap remove --purge firefox
 sudo snap remove --purge snap-store
 sudo snap remove --purge snapd-desktop-integration
@@ -11,11 +16,6 @@ sudo snap remove --purge bare
 sudo snap remove --purge snapd
 sudo apt remove -y --purge snapd
 sudo apt-mark hold snapd
-
-#
-echo 'Package: snapd
-Pin: release a=*
-Pin-Priority: -10' | sudo tee /etc/apt/preferences.d/nosnap.pref
 
 #
 sudo apt-mark hold gnome-software-plugin-snap
