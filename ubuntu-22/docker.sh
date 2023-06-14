1ff532d017f4c261d35ef1a40b8c90dabefb52ac
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-06-14 01:55:06 -0300

    openvpn3

--- ubuntu-22/docker.sh
+++ ubuntu-22/docker.sh
@@ -8,13 +8,12 @@ sudo apt-get update
 sudo apt-get install ca-certificates curl gnupg
 
 #
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker.gpg
+sudo chmod a+r /etc/apt/trusted.gpg.d/docker.gpg
 
 #
 echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/trusted.gpg.d/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
