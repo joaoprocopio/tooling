1bd924cf29b84942f8adf2541d522e156c268644
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-06-14 01:33:31 -0300

    pyenvg

---
+++ ubuntu-22/pyenv.sh
@@ -0,0 +1,17 @@
+#!/bin/env sh
+
+sudo apt update; sudo apt install --yes \
+  build-essential libssl-dev zlib1g-dev \
+  libbz2-dev libreadline-dev libsqlite3-dev curl \
+  libncursesw5-dev xz-utils tk-dev libxml2-dev \
+  libxmlsec1-dev libffi-dev liblzma-dev
+
+curl https://pyenv.run | bash
+
+echo '
+# pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+command -v pyenv > /dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+' >> ~/.zshrc
