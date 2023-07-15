31a4e08a44bdc824beef544a52c3a49b4af5393a
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-07-15 09:18:24 -0300

    remove

--- ubuntu-22/tools.sh
+++ ubuntu-22/tools.sh
@@ -9,18 +9,10 @@ read -p "Type here your work username: " WORK_USERNAME
 read -p "Type here your work email: " WORK_EMAIL
 
 #
-echo 'Useful software that cant be installed programatically
-https://www.google.com/intl/pt-BR/chrome/
-https://bitwarden.com/download/
-https://code.visualstudio.com/download/
-'
-
-#
 sudo apt update
 
 #
 sudo apt install --yes \
-  tmux htop neovim \
   git tree wget curl ffmpeg net-tools \
   gimp obs-studio qbittorrent vlc \
   gnome-tweaks dconf-editor gnome-shell-extensions \
