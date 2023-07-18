eafb1f11b3122e8b11f98132f09ee9b4199b6b7c
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-07-18 17:50:00 -0300

    postman

---
+++ ubuntu-22/postman.sh
@@ -0,0 +1,21 @@
+#!/bin/env sh
+
+#
+curl -fsSL https://dl.pstmn.io/download/latest/linux_64 --output postman.tar.gz
+
+#
+tar xzf postman.tar.gz
+
+#
+sudo mv Postman /opt/
+
+#
+echo "[Desktop Entry]
+Categories=Development;
+Comment=Supercharge your API workflow
+Exec="/opt/Postman/Postman"
+Icon=/opt/Postman/app/resources/app/assets/icon.png
+Name=Postman
+Terminal=false
+Type=Application
+Version=1.0" | tee ~/.local/share/applications/Postman.desktop >/dev/null
