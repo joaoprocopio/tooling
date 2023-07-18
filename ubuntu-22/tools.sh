821243cc9138b1752b36ab10c43d1de4455c3f94
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-07-18 18:58:29 -0300

    conf local

--- ubuntu-22/tools.sh
+++ ubuntu-22/tools.sh
@@ -34,7 +34,7 @@ curl -o ~/.config/nvim/init.lua https://raw.githubusercontent.com/nvim-lua/kicks
 git clone https://github.com/gpakosz/.tmux.git ~/.tmux
 ln -s -f ~/.tmux/.tmux.conf ~/.tmux.conf
 cp ~/.tmux/.tmux.conf.local ~/
-
+echo "set -g mouse on" | tee --append ~/.tmux.conf.local >/dev/null
 
 #
 touch ~/.gitignore
