11fac4b510f607b460261bf6bbffc362ac4993e0
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-07-18 18:57:25 -0300

    neovim

--- ubuntu-22/tools.sh
+++ ubuntu-22/tools.sh
@@ -17,7 +17,18 @@ sudo apt install --yes \
   git tree wget curl ffmpeg net-tools \
   gimp obs-studio qbittorrent vlc \
   gnome-tweaks dconf-editor gnome-shell-extensions \
+  ninja-build libtool autoconf automake cmake gcc make unzip patch gettext \
   libfuse2
+
+#
+git clone -b stable --depth 1 https://github.com/neovim/neovim ~/neovim
+cd ~/neovim
+make CMAKE_BUILD_TYPE=Release
+sudo make install
+rm -rf build
+sudo ln -s /usr/local/bin/nvim /usr/local/bin/vim
+mkdir ~/.config/nvim
+curl -o ~/.config/nvim/init.lua https://raw.githubusercontent.com/nvim-lua/kickstart.nvim/master/init.lua
 
 #
 git clone https://github.com/gpakosz/.tmux.git ~/.tmux
