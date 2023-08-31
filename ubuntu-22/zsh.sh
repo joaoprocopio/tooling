3ba5b02b5ba25d52c143f7ac91df9196fb442db6
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-08-31 12:17:56 -0300

    starshipOut

--- ubuntu-22/zsh.sh
+++ ubuntu-22/zsh.sh
@@ -8,18 +8,3 @@ sudo apt install --yes git zsh curl
 
 #
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-#
-curl -sS https://starship.rs/install.sh | sh
-
-#
-echo '
-# starship
-eval "$(starship init zsh)"
-' | tee --append ~/.zshrc >/dev/null
-
-#
-touch ~/.config/starship.toml
-
-#
-starship preset plain-text-symbols | tee --append ~/.config/starship.toml >/dev/null
