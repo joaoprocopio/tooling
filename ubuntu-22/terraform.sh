0dd7588e6fbfc662ba7b70a3423662fd7ee81a20
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-09-01 12:54:22 -0300

    vai2

--- ubuntu-22/terraform.sh
+++ ubuntu-22/terraform.sh
@@ -1,7 +1,18 @@
 #!/bin/env sh
 
 #
+sudo apt-get update
+
+#
+sudo apt-get install -y gnupg software-properties-common
+
+#
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+#
+gpg --no-default-keyring \
+  --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+  --fingerprint
 
 #
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" |
