faf0a1b07445066ba011bf09eab5a947b3520aee
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-09-01 13:07:31 -0300

    YES ANSIBLE

--- ubuntu-22/ansible.sh
+++ ubuntu-22/ansible.sh
@@ -4,10 +4,10 @@
 sudo apt update
 
 #
-sudo apt install software-properties-common
+sudo apt install --yes software-properties-common
 
 #
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 
 #
-sudo apt install ansible
+sudo apt install --yes ansible
