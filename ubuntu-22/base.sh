9a4f12ea0ee9bd392434adc9f46ca6c7a5b394c7
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-11-30 17:55:19 -0300

    libs python

--- ubuntu-22/base.sh
+++ ubuntu-22/base.sh
@@ -5,13 +5,16 @@ sudo apt update
 
 # Instale na internet:
 # Chrome, Obs, Gimp, Qbittorrent e VLC
-#
 sudo apt install --yes \
   htop \
   tree wget curl ffmpeg net-tools \
   gnome-tweaks dconf-editor gnome-shell-extensions \
-  ninja-build libtool autoconf automake cmake gcc make unzip patch gettext \
-  libfuse2
+  ninja-build build-essential libtool autoconf automake cmake gcc make unzip patch gettext \
+  libfuse2 libssl-dev zlib1g-dev \
+  libbz2-dev libreadline-dev libsqlite3-dev curl \
+  libncursesw5-dev xz-utils tk-dev libxml2-dev \
+  libxmlsec1-dev libffi-dev liblzma-dev \
+  libgdal-dev libpq-dev libldap2-dev libsasl2-dev
 
 #
 echo "\n# aliases
--- ubuntu-22/pyenv.sh
+++ ubuntu-22/pyenv.sh
@@ -4,14 +4,6 @@
 sudo apt update
 
 #
-sudo apt install --yes \
-  build-essential libssl-dev zlib1g-dev \
-  libbz2-dev libreadline-dev libsqlite3-dev curl \
-  libncursesw5-dev xz-utils tk-dev libxml2-dev \
-  libxmlsec1-dev libffi-dev liblzma-dev \
-  libgdal-dev libpq-dev
-
-#
 curl https://pyenv.run | bash
 
 #
