7026122323857fa51180652f774ca22eb0d95a77
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-12-03 12:37:30 -0300

    vai

--- ubuntu-22/git.sh
+++ ubuntu-22/git.sh
@@ -1,12 +1,4 @@
-#!/bin/env zsh
-
-#
-read -p "Type here your personal username: " PERSONAL_USERNAME
-read -p "Type here your personal email: " PERSONAL_EMAIL
-
-#
-read -p "Type here your work username: " WORK_USERNAME
-read -p "Type here your work email: " WORK_EMAIL
+#!/bin/env bash
 
 #
 sudo apt update
@@ -23,18 +15,15 @@ echo "**/.vscode/**" | tee ~/.gitignore >/dev/null
 
 #
 echo "[user]
-  name = $PERSONAL_USERNAME
-  email = $PERSONAL_EMAIL
+  name = joaoprocopio
+  email = joaovitorcprocopio@gmail.com
 [alias]
   st = status
+  cm = commit
+  co = checkout
   lg = log --graph --decorate --abbrev-commit --all --pretty=format:'%C(auto)%h%Creset %C(cyan)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'
 [init]
   defaultBranch = main
 [core]
   excludesfile = /home/$USER/.gitignore
   editor = code --wait" | tee ~/.gitconfig >/dev/null
-
-#
-if [ -n "$WORK_EMAIL" ] && [ -n "$WORK_USERNAME" ]; then
-  echo "alias clone='git clone --config user.name=\"$WORK_EMAIL\" --config user.email=\"$WORK_USERNAME\"'" | tee --append ~/.zshrc >/dev/null
-fi
