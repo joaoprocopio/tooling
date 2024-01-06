1d4eeee63623af7ff78af8d0290210d8a9c6adc8
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-01-06 10:53:57 -0300

    kubectl

---
+++ commons/kubectl.sh
@@ -0,0 +1,19 @@
+#!/bin/env zsh
+
+#
+sudo apt-get update
+
+#
+sudo apt-get install -y apt-transport-https ca-certificates curl
+
+#
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+#
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+#
+sudo apt-get update
+
+#
+sudo apt-get install -y kubectl
