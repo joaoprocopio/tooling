d92ccdfe87f5725f562b76b2b7b1f1a65670e437
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-01-15 17:29:11 -0300

    lib cursor

--- commons/_base.sh
+++ commons/_base.sh
@@ -19,7 +19,7 @@ sudo apt install --yes \
   ninja-build build-essential libtool autoconf automake cmake gcc make unzip patch gettext \
   libfuse2 libssl-dev zlib1g-dev \
   libbz2-dev libreadline-dev libsqlite3-dev \
-  libncursesw5-dev xz-utils tk-dev libxml2-dev \
+  libncursesw5-dev xz-utils tk-dev libxml2-dev libxcb-cursor0 \
   libxmlsec1-dev libffi-dev liblzma-dev \
   libgdal-dev libpq-dev libldap2-dev libsasl2-dev
 
