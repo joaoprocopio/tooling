402add1634a027d9a66acb294918a8d2c16ea91c
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-03-05 23:31:27 -0300

    warp terminal

---
+++ commons/warp.sh
@@ -0,0 +1,12 @@
+#!/bin/env zsh
+
+#
+sudo apt install --yes curl
+
+#
+curl -fsSL "https://releases.warp.dev/stable/v0.2024.03.05.08.02.stable_00/warp-terminal_0.2024.03.05.08.02.stable.00_amd64.deb" -o "warp.deb"
+sudo dpkg -i warp.deb
+
+#
+rm warp.deb
+
