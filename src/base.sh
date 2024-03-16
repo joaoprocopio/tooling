8fec1d838a3cfb46e142ddde385efca52b7551d5
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-03-16 00:58:44 -0300

    pkgs

--- src/base.sh
+++ src/base.sh
@@ -20,7 +20,7 @@ sudo apt install --yes \
   libfuse2 libssl-dev zlib1g-dev \
   libbz2-dev libreadline-dev libsqlite3-dev \
   libncursesw5-dev xz-utils tk-dev libxml2-dev libxcb-cursor0 \
-  libxmlsec1-dev libffi-dev liblzma-dev \
+  libxmlsec1-dev libffi-dev liblzma-dev ncurses-dev \
   libgdal-dev libpq-dev libldap2-dev libsasl2-dev \
   libasound2-dev libfontconfig-dev libwayland-dev libxkbcommon-x11-dev libzstd-dev \
-  libevent-dev bison byacc
+  libevent-dev bison byacc pkg-config
