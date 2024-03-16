2b7175eb2a1db05e3061daeb6032ac672b61bcb1
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-03-16 00:43:54 -0300

    warp url

--- src/warp.sh
+++ src/warp.sh
@@ -1,10 +1,13 @@
 #!/bin/env bash
 
 #
+export WARP_URL='https://releases.warp.dev/stable/v0.2024.03.12.08.02.stable_01/warp-terminal_0.2024.03.12.08.02.stable.01_amd64.deb'
+
+#
 sudo apt install --yes curl
 
 #
-curl -fsSL "https://releases.warp.dev/stable/v0.2024.03.05.08.02.stable_00/warp-terminal_0.2024.03.05.08.02.stable.00_amd64.deb" -o "warp.deb"
+curl -fsSL $WARP_URL -o "warp.deb"
 sudo dpkg -i warp.deb
 
 #
