e490a8c72bfe9207c92b47ea327e21f29a85a698
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-03-25 14:21:08 -0300

    zoom lib

--- src/base.sh
+++ src/base.sh
@@ -18,7 +18,7 @@ sudo apt install --yes \
   gnome-tweaks dconf-editor gnome-shell-extensions \
   ninja-build build-essential libtool autoconf automake cmake gcc make unzip patch gettext \
   libfuse2 libssl-dev zlib1g-dev \
-  libbz2-dev libreadline-dev libsqlite3-dev \
+  libbz2-dev libreadline-dev libsqlite3-dev libxcb-xtest0 \
   libncursesw5-dev xz-utils tk-dev libxml2-dev libxcb-cursor0 \
   libxmlsec1-dev libffi-dev liblzma-dev ncurses-dev \
   libgdal-dev libpq-dev libldap2-dev libsasl2-dev \
