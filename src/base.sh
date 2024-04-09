4d8618f50707119522afbf7199569652a5921e88
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-04-08 23:31:12 -0300

    vai vulkan

--- src/base.sh
+++ src/base.sh
@@ -24,5 +24,5 @@ sudo apt install --yes \
   libncursesw5-dev xz-utils tk-dev libxml2-dev libxcb-cursor0 \
   libxmlsec1-dev libffi-dev liblzma-dev ncurses-dev \
   libgdal-dev libpq-dev libldap2-dev libsasl2-dev \
-  libasound2-dev libfontconfig-dev libwayland-dev libxkbcommon-x11-dev libzstd-dev \
+  libasound2-dev libfontconfig-dev libwayland-dev libxkbcommon-x11-dev libzstd-dev libvulkan1 \
   libevent-dev bison byacc pkg-config
