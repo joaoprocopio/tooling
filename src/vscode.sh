c80178c54bf6ec2297511faa31124427bbfaab10
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-04-11 16:35:03 -0300

    download vscode

---
+++ src/vscode.sh
@@ -0,0 +1,13 @@
+#!/bin/env bash
+
+#
+sudo apt-get install --yes curl
+
+#
+curl -fsSL 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64' -o code.deb
+
+#
+sudo dpkg -i code.deb
+
+#
+rm code.deb
