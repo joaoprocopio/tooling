a042eb1f0d3db813a05db6bf2fdc82d932782f39
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-04-15 17:06:45 -0300

    enhance obs

--- scripts/obs.sh
+++ scripts/obs.sh
@@ -1,10 +1,13 @@
 #!/bin/env bash
 
 #
-sudo add-apt-repository --yes ppa:obsproject/obs-studio
+sudo apt update
 
 #
-sudo apt update
+sudo apt install --yes software-properties-common
+
+#
+sudo add-apt-repository --yes --update ppa:obsproject/obs-studio
 
 #
 sudo apt install --yes ffmpeg obs-studio
