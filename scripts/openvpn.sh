50fb751c09d9fe904292f310eb18b5957b86c51f
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-04-15 17:12:40 -0300

    newliners

--- scripts/openvpn.sh
+++ scripts/openvpn.sh
@@ -8,10 +8,11 @@ sudo apt install --yes apt-transport-https curl
 
 #
 mkdir -p /etc/apt/keyrings
-curl -sSfL https://packages.openvpn.net/packages-repo.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/openvpn.gpg
+curl -sSfL https://packages.openvpn.net/packages-repo.gpg | \
+  sudo gpg --dearmor -o /etc/apt/keyrings/openvpn.gpg
 sudo chmod a+r /etc/apt/keyrings/openvpn.gpg
 
-echo "deb [signed-by=/etc/apt/keyrings/openvpn.gpg] https://packages.openvpn.net/openvpn3/debian bookworm main" |
+echo "deb [signed-by=/etc/apt/keyrings/openvpn.gpg] https://packages.openvpn.net/openvpn3/debian bookworm main" | \
   sudo tee /etc/apt/sources.list.d/openvpn3.list >/dev/null
 
 #
