ca4e89ec4d8fc126f5dd77438fed0a42b7041b69
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-05-06 13:35:07 -0300

    reorganize base

--- scripts/base.sh
+++ scripts/base.sh
@@ -9,9 +9,15 @@ https://zoom.us/download
 sudo apt update
 
 
-# terminal tools
+# terminal interface tools
 sudo apt install --yes \
-  htop neovim tmux
+  htop
+
+# terminal cli tools
+sudo apt install --yes \
+  tree wget curl ffmpeg net-tools ninja-build build-essential \
+  libtool autoconf automake cmake gcc make unzip patch gettext \
+  bison byacc pkg-config
 
 # gui tools
 sudo apt install --yes \
@@ -21,15 +27,15 @@ sudo apt install --yes \
 sudo apt install --yes \
   gnome-tweaks dconf-editor gnome-shell-extensions
 
-#
+# fonts
 sudo apt install --yes \
-  tree wget curl ffmpeg net-tools \
-  ninja-build build-essential libtool autoconf automake cmake gcc make unzip patch gettext \
-  fonts-jetbrains-mono \
-  libfuse2 libssl-dev zlib1g-dev \
-  libbz2-dev libreadline-dev libsqlite3-dev libxcb-xtest0 \
-  libncursesw5-dev xz-utils tk-dev libxml2-dev libxcb-cursor0 \
-  libxmlsec1-dev libffi-dev liblzma-dev ncurses-dev \
-  libgdal-dev libpq-dev libldap2-dev libsasl2-dev \
-  libasound2-dev libfontconfig-dev libwayland-dev libxkbcommon-x11-dev libzstd-dev libvulkan1 \
-  libevent-dev bison byacc pkg-config
+  fonts-jetbrains-mono
+
+# libraries
+sudo apt install --yes \
+  libfuse2 libssl-dev zlib1g-dev libbz2-dev libreadline-dev \
+  libsqlite3-dev libxcb-xtest0 libncursesw5-dev xz-utils tk-dev \
+  libxml2-dev libxcb-cursor0 libxmlsec1-dev libffi-dev liblzma-dev \
+  ncurses-dev libgdal-dev libpq-dev libldap2-dev libsasl2-dev \
+  libasound2-dev libfontconfig-dev libwayland-dev libxkbcommon-x11-dev \
+  libzstd-dev libvulkan1 libevent-dev
