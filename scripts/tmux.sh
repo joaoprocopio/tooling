404367be74f61e0099dfdd9c7782fb09fa002b11
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-05-06 13:37:34 -0300

    add tmux

---
+++ scripts/tmux.sh
@@ -0,0 +1,14 @@
+#!/bin/env bash
+
+#
+git clone -b '3.4' --depth 1 'git@github.com:tmux/tmux.git' ~/tmux
+cd ~/tmux
+sh autogen.sh
+./configure
+make && sudo make install
+
+#
+git clone https://github.com/gpakosz/.tmux.git ~/.tmux
+ln -s -f ~/.tmux/.tmux.conf ~/.tmux.conf
+cp ~/.tmux/.tmux.conf.local ~/
+echo "set -g mouse on" | tee --append ~/.tmux.conf.local >/dev/null
