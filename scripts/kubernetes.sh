bddc1e8757e66eaf2f092a1446067c0587e4b6d1
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-07-02 00:03:49 -0300

    remove act add k8s version

--- scripts/kubernetes.sh
+++ scripts/kubernetes.sh
@@ -1,15 +1,17 @@
 #!/bin/env bash
 
+K8S_VERSION="v1.29"
+
 # k8s
 #
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
 
 #
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/$K8S_VERSION/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
 #
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/$K8S_VERSION/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 #
 sudo apt-get update
