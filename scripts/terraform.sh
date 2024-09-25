1cc86716117a42c9282121a87336f47af83cb32e
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-09-25 16:52:18 -0300

    terraform

--- scripts/terraform.sh
+++ scripts/terraform.sh
@@ -15,7 +15,7 @@ gpg --no-default-keyring \
   --fingerprint
 
 #
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" |
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
   sudo tee /etc/apt/sources.list.d/hashicorp.list >/dev/null
 
 #
