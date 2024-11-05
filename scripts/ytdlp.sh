118c1873b5768afa27a26194d3aeb39a68d5a462
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-11-05 13:14:29 -0300

    ytdlp

--- scripts/ytdlp.sh
+++ scripts/ytdlp.sh
@@ -1,5 +1,5 @@
 #!/bin/env bash
 
 #
-curl -L 'https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp' -o '~/.local/bin/yt-dlp'
+curl -L 'https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp' -o ~/.local/bin/yt-dlp
 chmod a+rx ~/.local/bin/yt-dlp
