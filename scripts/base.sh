fc28ab3da60d3b8afa9e1a7dd1c7ca36a91a32f5
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-11-29 16:27:47 -0300

    vai 7zip

--- scripts/base.sh
+++ scripts/base.sh
@@ -11,7 +11,7 @@ sudo apt install --yes \
 # terminal cli tools
 sudo apt install --yes \
   tree wget curl ffmpeg net-tools ninja-build build-essential \
-  libtool autoconf automake cmake gcc make unzip patch gettext \
+  libtool autoconf automake cmake gcc make unzip p7zip-full patch gettext \
   bison re2c byacc pkg-config libgraph-easy-perl # graph-easy, very useful
 
 # gui tools
