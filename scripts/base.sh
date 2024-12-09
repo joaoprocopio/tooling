d7c81a1a9e4e9c930133c1c351fa3edd3195d810
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-12-09 19:09:01 -0300

    add rsync

--- scripts/base.sh
+++ scripts/base.sh
@@ -10,7 +10,7 @@ sudo apt install --yes \
 
 # terminal cli tools
 sudo apt install --yes \
-  tree wget curl ffmpeg net-tools ninja-build build-essential \
+  rsync tree wget curl ffmpeg net-tools ninja-build build-essential \
   libtool autoconf automake cmake gcc make unzip p7zip-full patch gettext \
   bison re2c byacc pkg-config libgraph-easy-perl # graph-easy, very useful
 
