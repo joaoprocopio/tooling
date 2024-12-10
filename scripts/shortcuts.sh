499feb026ecbe95d7c80e973267562e1ba9b4821
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-12-10 01:12:01 -0300

    fix terminal shortcut

--- scripts/shortcuts.sh
+++ scripts/shortcuts.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 
 #
-dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/binding "'warp-terminal'"
-dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/command "'<Ctrl><Alt>T'"
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/binding "'<Control><Alt>t'"
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/command "'warp-terminal'"
 dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/name "'Open Terminal'"
