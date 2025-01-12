c4d12e323ce23f5788f5fa3095f2e2758e3130a7
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-01-12 20:04:27 -0300

    subshell

--- scripts/android.sh
+++ scripts/android.sh
@@ -2,13 +2,15 @@
 
 # studio
 # get the release name from the website
-MOST_RECENT_RELEASE_YEAR=curl -s https://developer.android.com/studio | \
+MOST_RECENT_RELEASE_YEAR=$(
+  curl -s https://developer.android.com/studio | \
   # find the linux 64 bit td html element, and catch the next 5 following lines
   grep -A 5 'Linux<br>(64-bit)' | \
   # extract only the file name
   grep -o 'android-studio-[0-9.]\+-linux' | \
   # extract the year from the filename
   sed 's/android-studio-\([0-9.]\+\)-linux/\1/'
+)
 
 DOWNLOAD_URL="https://redirector.gvt1.com/edgedl/android/studio/ide-zips/$MOST_RECENT_RELEASE_YEAR/android-studio-$MOST_RECENT_RELEASE_YEAR-linux.tar.gz"
 
