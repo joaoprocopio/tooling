f3a7add814454f408511e74e6d6eb9db404c5158
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-03-23 14:11:27 -0300

    update tmux

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 
 #
-git clone -b '3.4' --depth 1 'git@github.com:tmux/tmux.git' ~/tmux
+git clone -b '3.5' --depth 1 'git@github.com:tmux/tmux.git' ~/tmux
 cd ~/tmux
 sh autogen.sh
 ./configure
