dc0c700ecaad979a2a3d1c1580b6d454697e3c01
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-04-10 01:05:35 -0300

    update

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -9,25 +9,21 @@ sudo apt-get install --yes ca-certificates curl gnupg
 
 #
 sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 #
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
 
 #
 sudo apt-get update
 sudo apt-get install --yes \
   docker-ce docker-ce-cli containerd.io \
   docker-buildx-plugin docker-compose-plugin
-
-sudo apt-get install --yes \
- docker-ce docker-ce-cli \
- docker-buildx-plugin docker-compose-plugin \
- containerd.io
 
 # post installation
 sudo groupadd docker
--- scripts/gsettings.sh
+++ scripts/gsettings.sh
@@ -1,6 +1,8 @@
 #!/bin/env bash
 
 # Keybindings
+# TODO: fazer funcionar o alt + tab normalmente
+
 # List Keybindings
 gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/']"
 
