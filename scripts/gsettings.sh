0986fbe5d40f9ee902e318b810632086cdceedc9
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-04-13 14:33:39 -0300

    print

--- scripts/gsettings.sh
+++ scripts/gsettings.sh
@@ -15,6 +15,7 @@ gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/or
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "Open Terminal"
 
 # Keybind 1
+gsettings set org.gnome.shell.keybindings show-screenshot-ui "[]"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ binding "Print"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ command "flameshot gui"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ name "Take Screenshot"
