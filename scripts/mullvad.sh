31e01e8db530acd34df4c77f2a6f55c8013eaea0
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-05-24 23:58:26 -0300

    mullvad

---
+++ scripts/mullvad.sh
@@ -0,0 +1,11 @@
+#!/bin/env bash
+
+#
+sudo curl -fsSLo /usr/share/keyrings/mullvad-keyring.asc https://repository.mullvad.net/deb/mullvad-keyring.asc
+
+#
+echo "deb [signed-by=/usr/share/keyrings/mullvad-keyring.asc arch=$( dpkg --print-architecture )] https://repository.mullvad.net/deb/stable stable main" | sudo tee /etc/apt/sources.list.d/mullvad.list
+
+#
+sudo apt update
+sudo apt install mullvad-vpn
