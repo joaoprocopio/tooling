62c69aed5f0122a01596435dbf4a0b1d5c94ea52
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-06-21 19:14:00 -0300

    vai

--- scripts/rust.sh
+++ scripts/rust.sh
@@ -1,9 +1,17 @@
 #!/bin/env bash
 
 #
-curl https://sh.rustup.rs -sSf | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 #
 echo '
 # rust
 . "$HOME/.cargo/env"' | tee --append ~/.zshrc >/dev/null
+
+#
+source ~/.zshrc
+
+#
+rustup update
+rustup component add rust-analyzer
+
