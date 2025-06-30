cb097b10ebb67a4b27cef5f69a0e576d039fe479
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-06-30 20:17:19 -0300

    awscli

--- scripts/awscli.sh
+++ scripts/awscli.sh
@@ -1,5 +1,10 @@
 #!/bin/env bash
 
 #
-sudo apt-get update
-sudo apt-get install --yes awscli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
+
+#
+rm -rf aws
+rm awscliv2.zip
