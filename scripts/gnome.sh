8381b5dc7da85c56a61f3a54b772a0969560eaf6
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-07-04 17:46:45 -0300

    lesgo

--- scripts/gnome.sh
+++ scripts/gnome.sh
@@ -2,5 +2,8 @@
 
 # TODO: automar a parte de configurar o gnome-tweaks e as extensions
 # TODO: NOTA: sobre o appindicator precisa ativar dentro do app de extensões
+# TODO: NOTA: precisa ativar o user themes, pra dentro do gnome tweaks trocar o Appearance > Legacy Applications > Adwaita-dark
+# TODO: NOTA: colocar no dark mode
+# TODO: NOTA: desabilitar application search e indexing
 sudo apt install --yes \
   dconf-editor gnome-tweaks gnome-shell-extensions gnome-shell-extension-appindicator
