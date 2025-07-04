63e381fa526270de75fc6ddf2b97bc30a5559096
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-07-04 00:58:22 -0300

    update

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -29,6 +29,6 @@ wget -O- "https://keyserver.ubuntu.com/pks/lookup?fingerprint=on&op=get&search=0
 echo "deb [signed-by=/usr/share/keyrings/ansible-archive-keyring.gpg] http://ppa.launchpad.net/ansible/ansible/ubuntu $UBUNTU_CODENAME main" | \
     sudo tee /etc/apt/sources.list.d/ansible.list
 
-# it will asks for your input, but in a buggy way, just press enter, you'll see a error.
+# it will asks for your input, but in a buggy way, just press enter, you will see a error.
 sudo apt update
 sudo apt install ansible
