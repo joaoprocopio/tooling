039ac36311310ca590a2f52d50a270ea5a678677
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-07-06 00:25:05 -0300

    lesgo

--- snippets/increase-swapmem.sh
+++ snippets/increase-swapmem.sh
@@ -1,8 +1,20 @@
 #!/bin/env bash
 
+# https://docs.vultr.com/how-to-add-swap-memory-on-debian-12
+
 #
 sudo swapoff -a
-sudo dd if=/dev/zero of=/swapfile bs=1G count=8
+# if you ram is in between 8GB and 64GB, the swapfile size should be 1.5 time the amount of total RAM
+sudo fallocate -l 24G /swapfile
+sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
-grep SwapTotal /proc/meminfo
+
+#
+sudo swapon -s
+
+#
+sudo cp /etc/fstab /etc/fstab.bak
+
+# add to `/etc/fstab`
+# UUID=abf9a9b8-d717-4e42-9000-2e01935100cc /swapfile swap swap defaults 0 0
