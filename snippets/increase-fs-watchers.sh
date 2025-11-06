61e84e3d283216907e7c1dd0970b533fffbd9abc
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-11-06 15:38:18 +0000

    update snippet

--- snippets/increase-fs-watchers.sh
+++ snippets/increase-fs-watchers.sh
@@ -1,9 +1,10 @@
 #!/bin/env bash
-
 # https://stackoverflow.com/a/55763478
+# https://www.debian.org/releases/trixie/release-notes/issues.html#etc-sysctl-conf-is-no-longer-honored
 
-# insert the new value into the system config
-echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+# replace the value in the system config
+sudo sed -i 's/fs.inotify.max_user_watches = 65536/fs.inotify.max_user_watches=524288/g' /usr/lib/sysctl.d/30-localsearch.conf && \
+    sudo sysctl -p
 
 # check that the new value was applied
 cat /proc/sys/fs/inotify/max_user_watches
