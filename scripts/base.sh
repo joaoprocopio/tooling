0be22887a082af30ffab8b7f4a8b870f9baf95b8
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-11-19 02:20:28 +0000

    Update base.sh

--- scripts/base.sh
+++ scripts/base.sh
@@ -9,7 +9,7 @@ sudo apt install --yes \
 
 # terminal cli tools
 sudo apt install --yes \
-  git blueprint-compiler rsync jq tree wget curl ffmpeg net-tools ninja-build build-essential \
+  ssh git blueprint-compiler rsync jq tree wget curl ffmpeg net-tools ninja-build build-essential \
   libtool autoconf automake cmake gcc make unzip p7zip-full patch gettext \
   bison re2c byacc pkg-config libgraph-easy-perl # graph-easy, very useful
 
