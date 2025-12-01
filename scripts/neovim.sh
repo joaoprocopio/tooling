78340b610eedb455fe7e7850d2b2ba9493336548
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-12-01 23:08:47 +0000

    neovim

--- scripts/neovim.sh
+++ scripts/neovim.sh
@@ -1,7 +1,15 @@
 #!/bin/env bash
 
-#
-sudo apt install neovim
+# pre-requisites
+sudo apt-get install ninja-build gettext cmake unzip curl
 
 #
-git clone https://github.com/nvim-lua/kickstart.nvim.git ~/.config/nvim
+git clone -b stable --depth 1 https://github.com/neovim/neovim ~/neovim
+cd ~/neovim
+make CMAKE_BUILD_TYPE=Release
+sudo make install
+rm -rf build
+sudo ln -s /usr/local/bin/nvim /usr/local/bin/vim
+
+#
+git clone --depth 1 https://github.com/nvim-lua/kickstart.nvim.git ~/.config/nvim
