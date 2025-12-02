6caff77eb80987b969c9f55f639d500bcdcec044
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-12-02 19:31:48 +0000

    vai

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -8,3 +8,7 @@ alias k="kubectl"
 alias dk="docker"
 alias dkc="docker compose"
 alias ll="ls -lha"' | tee --append ~/.zshrc >/dev/null
+
+echo '
+# local bin
+export PATH="$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH"' | tee --append ~/.zshrc >/dev/null
