74422717011e18ff57de062bb93b3fb4e177e355
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-12-08 17:23:56 +0000

    wip

--- scripts/ghostty.sh
+++ scripts/ghostty.sh
@@ -1,29 +1,32 @@
 #!/bin/env bash
 
-# https://ziglang.org/download/index.json
-ZIG_VERSION="0.13.0"
-ZIG_ARCHITECTURE="linux-x86_64"
-ZIG_DIRNAME="zig-$ZIG_ARCHITECTURE-$ZIG_VERSION"
-ZIG_TARBALL="$ZIG_DIRNAME.tar.xz"
-ZIG_TARBALL_URL="https://ziglang.org/download/$ZIG_VERSION/$ZIG_TARBALL"
+#
+sudo apt install --yes minisign
+
+# all variables are prefixed to prevent namespace clashing
+GHOSTTY_PUBLIC_KEY="RWQlAjJC23149WL2sEpT/l0QKy7hMIFhYdQOFy0Z7z7PbneUgvlsnYcV"
+GHOSTTY_VERSION="1.2.3"
+GHOSTTY_DIR="ghostty-$GHOSTTY_VERSION"
+GHOSTTY_TAR_GZ_FILE="$GHOSTTY_DIR.tar.gz"
 
 #
-curl "$ZIG_TARBALL_URL" -o "$ZIG_TARBALL"
-tar -xvf "$ZIG_TARBALL"
-rm "$ZIG_TARBALL"
+curl -#fSL "https://release.files.ghostty.org/$GHOSTTY_VERSION/$GHOSTTY_TAR_GZ_FILE" -o $GHOSTTY_TAR_GZ_FILE
+
+# verifies the signature
+minisign -Vm $GHOSTTY_TAR_GZ_FILE -P $GHOSTTY_PUBLIC_KEY
 
 #
-mv "$ZIG_DIRNAME" "$HOME/zig"
-export PATH="$PATH:$HOME/zig"
-
-# https://ghostty.org/docs/install/build#linux-installation-tips
-GHOSTTY_RELEASE='v1.1.3'
-git clone -b $GHOSTTY_RELEASE --depth 1 https://github.com/ghostty-org/ghostty ~/ghostty
-cd ~/ghostty
+tar -xvf $GHOSTTY_TAR_GZ_FILE
+rm $GHOSTTY_TAR_GZ_FILE
 
 #
-zig build -p "$HOME/.local" -Doptimize=ReleaseFast
-cd ~
-rm -rf ~/ghostty
-rm -rf ~/zig
-sudo mv ~/.local/bin/ghostty /usr/local/bin/
+if [ ! -d $GHOSTTY_DIR ]; then
+    echo "Directory $GHOSTTY_DIR does not exist" >&2
+    exit 1
+fi
+
+# zig build -p "$HOME/.local" -Doptimize=ReleaseFast
+# cd ~
+# rm -rf ~/ghostty
+# rm -rf ~/zig
+# sudo mv ~/.local/bin/ghostty /usr/local/bin/
