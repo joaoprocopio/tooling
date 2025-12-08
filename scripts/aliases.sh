ecfdb8fffe57ee6d9d35a484851fd92f2bbf540b
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-12-08 22:00:32 +0000

    vai

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -11,4 +11,4 @@ alias ll="ls -lha"' | tee --append ~/.zshrc >/dev/null
 
 echo '
 # local bin
-export PATH="$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH"' | tee --append ~/.zshrc >/dev/null
+export PATH="$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH"' | tee --append ~/.zshrc --append ~/.bashrc >/dev/null
--- scripts/nvm.sh
+++ scripts/nvm.sh
@@ -3,7 +3,7 @@
 #
 sudo apt install --yes curl
 
-#
+# https://github.com/nvm-sh/nvm?tab=readme-ov-file#installing-and-updating
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 
 #
