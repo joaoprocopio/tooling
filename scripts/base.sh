256c1cf6595f553a36857bce0e25fcc559158615
joaoprocopio <joaovitorcprocopio@gmail.com>
2026-06-02 17:30:37 +0100

    libsoup

--- scripts/base.sh
+++ scripts/base.sh
@@ -32,4 +32,5 @@ sudo apt install --yes \
   libzstd-dev libvulkan1 libevent-dev \
   libflac-dev libsdl2-dev \
   libgtk-4-dev libadwaita-1-dev libgtk4-layer-shell-dev \
-  libapr1-dev libaprutil1-dev libglib2.0-dev libc6-dbg
+  libapr1-dev libaprutil1-dev libglib2.0-dev libc6-dbg \
+  libsoup-3.0-dev
